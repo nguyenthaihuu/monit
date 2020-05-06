87773d45ba17e4a633bf109edf2bf29654300f0d
nguyenthaihuu <42740589+nguyenthaihuu@users.noreply.github.com>
2020-05-06 15:45:05 +0700

    Update install_monit.sh

--- install_monit.sh
+++ install_monit.sh
@@ -22,3 +22,21 @@ chkconfig monit on
 
 # monit status
 service monit status
+
+# 
+cd telegram
+
+# Put `sendtelegram.sh` and `monit2telegram.sh` to `/usr/local/bin` and make them executable.
+cp telegramrc /etc/telegramrc
+cp sendtelegram.sh /usr/local/bin/sendtelegram
+chmod +x /usr/local/bin/sendtelegram
+cp monit2telegram.sh /usr/local/bin/monit2telegram
+chmod +x /usr/local/bin/monit2telegram
+
+
+# ```nginx
+# check file nginx.pid with path /var/run/nginx.pid
+#     if changed pid then exec "/usr/local/bin/monit2telegram"
+# ```
+
+
